dfcfca0affc956c3b50c36a203a49578088d45fb
zhangbo2008 <15122306087@163.com>
2019-09-18 18:36:13 +0800

    12

--- audio_processing.py
+++ audio_processing.py
@@ -6,7 +6,7 @@ import librosa.util as librosa_util
 
 def window_sumsquare(window, n_frames, hop_length=200, win_length=800,
                      n_fft=800, dtype=np.float32, norm=None):
-    # 一针总共800长度,n:总共解析多少个针
+    # 总共800长度,n:总共解析多少个针
     """
     # from librosa 0.6
     Compute the sum-square envelope of a window function at a given hop length.
@@ -42,7 +42,7 @@ def window_sumsquare(window, n_frames, hop_length=200, win_length=800,
     if win_length is None:
         win_length = n_fft
 
-    n = n_fft + hop_length * (n_frames - 1)
+    n = n_fft + hop_length * (n_frames - 1) #总长
     x = np.zeros(n, dtype=dtype)
 
     # Compute the squared window at the desired length
@@ -51,13 +51,11 @@ def window_sumsquare(window, n_frames, hop_length=200, win_length=800,
     win_sq = librosa_util.pad_center(win_sq, n_fft) #填充0. 结果长度是n_fft,如果win_length指定了,
     #那么这行代码彩旗效果.
 
-    # Fill the envelope
-    for i in range(n_frames):
+    # Fill the envelope#下一个函数进行函数波形每次的偏右200然后叠加的运算.所以叫sum_square
+    for i in range(n_frames): #hop_length 表示跳过的大小.就是静音时间段的长度.
         sample = i * hop_length
         x[sample:min(n, sample + n_fft)] += win_sq[:max(0, min(n_fft, n - sample))]
     return x
-
-
 def griffin_lim(magnitudes, stft_fn, n_iters=30):
     """
     PARAMS
@@ -74,7 +72,7 @@ def griffin_lim(magnitudes, stft_fn, n_iters=30):
     for i in range(n_iters):
         _, angles = stft_fn.transform(signal)
         signal = stft_fn.inverse(magnitudes, angles).squeeze(1)
-    return signal
+    return signal #得到一个噪音波.
 
 
 def dynamic_range_compression(x, C=1, clip_val=1e-5):
@@ -83,7 +81,7 @@ def dynamic_range_compression(x, C=1, clip_val=1e-5):
     ------
     C: compression factor
     """
-    return torch.log(torch.clamp(x, min=clip_val) * C)
+    return torch.log(torch.clamp(x, min=clip_val) * C)#向量裁剪
 
 
 def dynamic_range_decompression(x, C=1):
@@ -92,4 +90,4 @@ def dynamic_range_decompression(x, C=1):
     ------
     C: compression factor used to compress
     """
-    return torch.exp(x) / C
+    return torch.exp(x) / C #裁剪逆运算.
---
+++ test.py
@@ -0,0 +1,114 @@
+#%% md
+'''
+通过与新联模型开始test
+
+'''
+## Tacotron 2 inference code
+#Edit the variables **checkpoint_path** and **text** to match yours and run the entire code to generate plots of mel outputs, alignments and audio synthesis from the generated mel-spectrogram using Griffin-Lim.
+
+#%% md
+
+#### Import libraries and setup matplotlib
+
+#%%
+
+import matplotlib
+
+import matplotlib.pylab as plt
+
+import IPython.display as ipd
+
+import sys
+sys.path.append('waveglow/')
+import numpy as np
+import torch
+
+from hparams import create_hparams
+from model import Tacotron2
+from layers import TacotronSTFT, STFT
+from audio_processing import griffin_lim
+from train import load_model
+from text import text_to_sequence
+from denoiser import Denoiser
+
+#%%
+
+def plot_data(data, figsize=(16, 4)):
+    fig, axes = plt.subplots(1, len(data), figsize=figsize)
+    for i in range(len(data)):
+        axes[i].imshow(data[i], aspect='auto', origin='bottom',
+                       interpolation='none')
+
+#%% md
+
+#### Setup hparams
+
+#%%
+
+hparams = create_hparams()
+hparams.sampling_rate = 22050
+
+#%% md
+
+#### Load model from checkpoint
+
+#%%
+
+checkpoint_path = "tacotron2_statedict.pt"
+model = load_model(hparams)
+model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+_ = model.cuda().eval().half()
+
+#%% md
+
+#### Load WaveGlow for mel2audio synthesis and denoiser
+
+#%%
+
+waveglow_path = 'waveglow_256channels.pt'
+waveglow = torch.load(waveglow_path)['model']
+waveglow.cuda().eval().half()
+for k in waveglow.convinv:
+    k.float()
+denoiser = Denoiser(waveglow)
+
+#%% md
+
+#### Prepare text input
+
+#%%
+
+text = "Waveglow is really awesome!"
+sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
+sequence = torch.autograd.Variable(
+    torch.from_numpy(sequence)).cuda().long()
+
+#%% md
+
+#### Decode text input and plot results
+
+#%%
+
+mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+plot_data((mel_outputs.float().data.cpu().numpy()[0],
+           mel_outputs_postnet.float().data.cpu().numpy()[0],
+           alignments.float().data.cpu().numpy()[0].T))
+
+#%% md
+
+#### Synthesize audio from spectrogram using WaveGlow
+
+#%%
+
+with torch.no_grad():
+    audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+ipd.Audio(audio[0].data.cpu().numpy(), rate=hparams.sampling_rate)
+
+#%% md
+
+#### (Optional) Remove WaveGlow bias
+
+#%%
+
+audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+ipd.Audio(audio_denoised.cpu().numpy(), rate=hparams.sampling_rate)
